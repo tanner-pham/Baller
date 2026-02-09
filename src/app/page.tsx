be8cd58ba5d03022d17a61aba30f0aabc254d673
Tanner Pham <Tannerpham64@gmail.com>
2026-02-09 10:53:53 -0800

    Readjusted folder layout and imported styles from figma

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Navigation } from './components/Navigation';
+import { Hero } from './components/Hero';
+import { HowItWorks } from './components/HowItWorks';
+import { Features } from './components/Features';
+import { FinalCTA } from './components/FinalCTA';
+import { Footer } from './components/Footer';
+
+export default function App() {
+  return (
+    <div className="size-full overflow-y-auto bg-[#F5F5F0]">
+      <Navigation />
+      <Hero />
+      <HowItWorks />
+      <Features />
+      <FinalCTA />
+      <Footer />
+    </div>
+  );
+}
