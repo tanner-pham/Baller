166abe0f2078c3c2ad77536b60055b4c50bdc3ac
David <davidguzmanvalente@gmail.com>
2026-02-11 22:42:35 -0800

    feat: added the price-analysis, with proper Props for future implmementation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { CurrentListing } from './(components)/CurrentListing';
+import { PricingAnalysis } from './(components)/PriceAnalysis';
 
 export default function DashboardPage() {
   return (
@@ -17,6 +18,16 @@ export default function DashboardPage() {
           sellerName="John Doe"
         />
       </div>
+      <PricingAnalysis
+        suggestedOffer='180'
+        modelAccuracy='90'
+        marketValue='230'
+        topReasons= {['Battery health/cycle count isn\'t listed, and battery replacement can be a real cost',
+          'A 2018 laptop is 6-7 years old, and even in great condition it\'s closer to end-of-support years than newer models',
+          '256 GB is usable but relatively small by today\s standards',
+          'Similar Intel 2018 15" listings vary a lot; you’re offering a fair midpoint that reflects that range'
+        ]}
+        negotiationTip={'Make it easy to say yes. Pair a reasonable offer with a clear, low-friction close: "Would you take $180? I can meet today, I\'m ready to pay immediately, and I can come to a spot that\'s convenient for you"'}></PricingAnalysis>
     </main>
   );
 }
